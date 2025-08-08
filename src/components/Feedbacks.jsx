fddd6911d9d94b976e072e6221a09afbba4ba48e
Afiniki <johnafinikimhya12@gmail.com>
2025-08-08 16:59:25 +0100

    Completion of testimonials and contacts section, along with stars effect bg and 3d earth globe

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import { styles } from "../styles";
+import { motion } from "framer-motion";
+import { SectionWrapper } from "../hoc";
+import { testimonials } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
+const FeedbackCard = ({
+  index,
+  testimonial,
+  designation,
+  name,
+  company,
+  image,
+}) => {
+  return (
+    <motion.div
+      variants={fadeIn("", "spring", index * 0.5, 0.75)}
+      className="bg-black-200 p-10 rounded-3xl w-full xs:w-[320px] "
+    >
+      <p className=" text-white text-6xl  ">"</p>
+      <div className="mt-1">
+        <p className=" text-white tracking-wider text-lg ">{testimonial}</p>
+        <div className=" mt-7 flex justify-between items-center gap-1 ">
+          <div className="flex flex-1 flex-col">
+            <p className=" text-white font-medium text-base ">
+              <span className=" blue-text-gradient ">@{name}</span>
+            </p>
+            <p className=" text-secondary text-sm mt-1 ">
+              {designation} of {company}
+            </p>
+          </div>
+          <img src={image} alt={`Feedback-from-${name}`} className="w-10 h-10 rounded-full object-cover" />
+        </div>
+      </div>
+    </motion.div>
+  );
+};
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
-  )
-}
+    <div className=" mt-12 bg-black-100 rounde-[20px] ">
+      <div
+        className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px] `}
+      >
+        <motion.div variants={textVariant()}>
+          <p className={`${styles.sectionSubText}`}>What other say</p>
+          <h2 className={`${styles.sectionHeadText}`}>Testimonials.</h2>
+        </motion.div>
+      </div>
+      <div className={`${styles.paddingX} -mt-20 flex flex-wrap pb-14 gap-7 `}>
+        {testimonials.map((testimonial, index) => (
+          <FeedbackCard key={testimonial.name} index={index} {...testimonial} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Feedbacks
+export default SectionWrapper(Feedbacks, "");
--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+// import { Stars } from '@react-three/drei'
+import { Canvas, useFrame } from "@react-three/fiber";
+import React, { Suspense, useState, useRef } from "react";
+import * as random from "maath/random/dist/maath-random.esm";
+import { PointMaterial, Preload, Points } from "@react-three/drei";
 
-const Stars = () => {
+const Stars = (props) => {
+  const ref = useRef();
+  const [sphere] = useState(() =>
+    random.inSphere(new Float32Array(5000), { radius: 1.2 }));
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+    ref.current.rotation.z -= delta / 20;
+  })
   return (
-    <div>Stars</div>
-  )
-}
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+        <PointMaterial
+          transparent
+          color="#f272c8"
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+          // toneMapped={false}
+        />
+      </Points>
+      <Preload all />
+    </group>
+  );
+};
 
-export default Stars
+const StarsCanvas = () => {
+  return (
+    <div className="w-full h-auto absolute inset-0 z-[-1]">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+
+        <Preload all />
+      </Canvas>
+    </div>
+  );
+};
+
+export default StarsCanvas;
